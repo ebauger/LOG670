016934db450d02fe6399cb0297a9d03170240a02
log320nick <nickcolocase@gmail.com>
2015-02-14 21:38:18 -0500

    Ajout programme generation soil

---
+++ Lab1/CodeJavaGenerationSoil/fusion.java
@@ -0,0 +1,13 @@
+package filecreator;
+
+public class fusion {
+
+	public void execute() {
+		mainapp m = new mainapp();
+		
+		m.createPlusieursVoiture(0, 4);
+		m.createUnPlatoon(0, 0, 2);
+		m.createUnPlatoon(1, 2, 2);
+		
+	}
+}
